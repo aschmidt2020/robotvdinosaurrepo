9240f8b9bbba4a35e2ab0f1544ca773349a1f45d
Audrey Schmidt <audsch@outlook.com>
2021-11-11 09:47:11 -0600

    created dinosaur class with init parameters and empty dino_attack method

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, dino_name, attack_power):
+        self.dino_name = dino_name
+        self.attack_power = attack_power
+        self.dino_health = 100
+    
+    def dino_attack(self, robot):
+        pass
