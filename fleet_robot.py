20f6161e5403c92a264e0d6f43eeb153527d43fb
Audrey Schmidt <audsch@outlook.com>
2021-11-11 10:24:40 -0600

    instantiated 3 robots and appended to robot list

--- fleet_robot.py
+++ fleet_robot.py
@@ -1,6 +1,8 @@
+from robot import robot_list #imported list from other file
+
 class Fleet:
     def __init__(self):
         self.robot_list = []
         
     def create_robo_fleet(self):
-        pass
+        self.robot_list = robot_list
--- robot.py
+++ robot.py
@@ -1,10 +1,21 @@
 from weapon import Weapon
 
 class Robot:
-    def __init__(self, robot_name):
+    def __init__(self, robot_name, weapon_name, attack_power):
         self.robot_name = robot_name
         self.robot_health = 100
-        self.robot_weapon = Weapon()
-    
+        self.robot_weapon = Weapon(weapon_name, attack_power)
+        
     def robo_attack(self, dinosaur):
         pass
+
+#instantiated three robots and added to list
+robot_list = [ ]
+
+robot_one = Robot('Robot One', 'Basic Weapon One', 10)
+robot_two = Robot('Robot Two', 'Basic Weapon One', 10)
+robot_three = Robot('Robot Three', 'Basic Weapon One', 10)
+
+robot_list.append(robot_one)
+robot_list.append(robot_two)
+robot_list.append(robot_three)
