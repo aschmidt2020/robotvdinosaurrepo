4535df47ef20a0b5c317d381db32e251923f431a
Audrey Schmidt <audsch@outlook.com>
2021-11-11 10:30:15 -0600

    instantiated 3 dinos and appended to dino_list

--- dinosaur.py
+++ dinosaur.py
@@ -6,3 +6,14 @@ class Dinosaur:
     
     def dino_attack(self, robot):
         pass
+
+#instantiated three dinos and added to list
+dino_list = [ ]
+
+dinosaur_one = Dinosaur('Dino One', 10)
+dinosaur_two = Dinosaur('Dino Two', 10)
+dinosaur_three = Dinosaur('Dino Three', 10)
+
+dino_list.append(dinosaur_one)
+dino_list.append(dinosaur_two)
+dino_list.append(dinosaur_three)
--- herd_dino.py
+++ herd_dino.py
@@ -1,6 +1,8 @@
+from dinosaur import dino_list
+
 class Herd:
     def __init__(self):
         self.dino_list = [ ]
     
     def create_dino_herd(self):
-        pass
+        self.dino_list = dino_list
